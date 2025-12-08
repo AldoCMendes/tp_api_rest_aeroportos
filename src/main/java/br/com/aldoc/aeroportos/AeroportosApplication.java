cbe7b07f335d780c6a25671570bcbc89a514d891
AldoCMendes <aldoconstantinomendes@gmail.com>
2025-12-08 19:33:42 -0300

    feat: inicializa projeto Spring Boot com Maven

---
+++ src/main/java/br/com/aldoc/aeroportos/AeroportosApplication.java
@@ -0,0 +1,12 @@
+package br.com.aldoc.aeroportos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroportosApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AeroportosApplication.class, args);
+  }
+}
+
